6bf08bf27cda82c38ce6fa85c9c1ea3aecc7e567
Luís Felipe de Souza <66641151+lfwdev@users.noreply.github.com>
2021-11-16 08:57:49 -0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>&nbsp; | &nbsp;
+    <router-link to="/about">About</router-link>&nbsp; | &nbsp;
+    <router-link to="/row-col">Row-col</router-link>
   </div>
   <router-view/>
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="alert alert-info bg-transparent">
+            <h1>This is an about page</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
