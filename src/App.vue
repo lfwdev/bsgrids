87718a730550c1dcceb20eb6289f08d75da902a3
Luís Felipe de Souza <66641151+lfwdev@users.noreply.github.com>
2021-11-16 11:13:48 -0300

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link>&nbsp; | &nbsp;
-    <router-link to="/about">About</router-link>&nbsp; | &nbsp;
-    <router-link to="/playground">Playground</router-link>&nbsp; | &nbsp;
-    <router-link to="/row-col">Row-col</router-link>
+    <!-- <router-link to="/about">About</router-link>&nbsp; | &nbsp; -->
+    <router-link to="/playground">Playground</router-link>
+    <!-- &nbsp; | &nbsp; -->
+    <!-- <router-link to="/row-col">Row-col</router-link> -->
   </div>
   <router-view/>
 </template>
