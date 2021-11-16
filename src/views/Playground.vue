cf9ba4aaa0a52f4b3ccfe19563a8cc802cb9df22
Luís Felipe de Souza <66641151+lfwdev@users.noreply.github.com>
2021-11-16 11:12:39 -0300

    made interative playground

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,38 +1,84 @@
 <template>
-  <div class="container">
-      <div class="row py-2">
-          <div class="bg-dark col">
-              &nbsp;
-          </div>
-      </div>
-      <div class="row row-cols-12 gx-2">
-          <template v-for="n in 16" :key="'demo-' + n">
-            <div :class="getRandomBorderColor()" class="bg-dark text-white border border-5 col p-1">
-                <b>
-                    &nbsp; {{ n }} &nbsp; 
-                </b>
+    <div>
+        <div class="controls container">
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text"># Items</span>
+                <input type="text" v-model="value" class="form-control">
             </div>
-          </template>
-      </div>
-  </div>
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text">Container Classes</span>
+                <input type="text" v-model="containerClasses" class="form-control">
+            </div>
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text">Container Style</span>
+                <input type="text" v-model="containerStyle" class="form-control">
+            </div>
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text">Row Classes¹</span>
+                <input type="text" v-model="rowClasses" class="form-control">
+            </div>
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text">Row Style</span>
+                <input type="text" v-model="rowStyle" class="form-control">
+            </div>
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text">Item Classes</span>
+                <input type="text" v-model="itemClasses" class="form-control">
+            </div>
+            <div class="input-group input-group-sm mb-3">
+                <span class="input-group-text">Item Style</span>
+                <input type="text" v-model="itemStyle" class="form-control">
+            </div>
+            <div class="alert info">
+                <small>
+                    ¹ Parent container needs matching padding utility to use gutter on the X axis!
+                </small>
+            </div>
+        </div>
+        <div :class="containerClasses" :style="containerStyle"> 
+            <div :class="rowClasses" :style="rowStyle">
+                <template v-for="n in getNumberOfItems()" :key="'demo-' + n">
+                    <div :class="itemClasses + ' ' + getRandomBorderColor()" :style="itemStyle">
+                        <div class="bg-dark text-center text-white p-1">
+                            <b>
+                                {{ n }}
+                            </b>
+                        </div>
+                    </div>
+                </template>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     data: function() {
         return {
+            containerClasses: 'container px-3',
+            containerStyle: 'border: 1px dashed red;',
+            rowClasses: 'row row-cols-12 gx-0',
+            rowStyle: 'border: 1px dashed cyan;',
+            itemClasses: 'border border-5 col-1',
+            itemStyle: 'border-style: inset !important;',
+            value: 48,
             bgColors: ['bg-primary','bg-success','bg-danger','bg-warning','bg-secondary','bg-info'],
             borderColors: ['border-primary','border-success','border-danger','border-warning','border-secondary','border-info']
         }
     },
     methods: {
+        getNumberOfItems(){
+            let val = this.value * 1;
+            return (val < 255 ? val : 255) ;
+        },
+        getRandomInt(max){
+            return Math.floor(((Math.random() * max)));
+        },
         getRandomBgColor(){
-            let n = Math.floor(((Math.random() * 5)));
-            return this.bgColors[n];
+            return this.bgColors[this.getRandomInt(5)];
         },
         getRandomBorderColor(){
-            let n = Math.floor(((Math.random() * 5)));
-            return this.borderColors[n];
+            return this.borderColors[this.getRandomInt(5)];
         }
     }
 }
